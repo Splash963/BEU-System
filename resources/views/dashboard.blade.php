91d4ee27254a8c58ba9d3ed3568f2c7725cb01d9
Splash963 <theekshanahirushan10@gmail.com>
2025-11-17 16:24:06 +0530

    Designing Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,54 @@
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+<style>
+    .quick-links {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .card {
+        width: 25rem;
+        height: 10rem;
+        border-radius: 0;
+    }
+
+    .circle-card {
+        width: 25rem;
+        height: 29rem;
+        border-radius: 0;
+    }
+
+    .chart-card {
+        width: 80.3rem;
+        height: 13rem;
+        border-radius: 0;
+    }
+
+    .charts {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    @media(max-width:768px) {
+        .quick-links {
+            flex-wrap: wrap;
+        }
+
+        .chart-card {
+            width: 22rem;
+        }
+
+        .charts {
+            flex-wrap: wrap;
+        }
+
+        .circle-card {
+            width: 22rem;
+        }
+
+    }
+</style>
 
 <body>
 
@@ -16,26 +64,72 @@
     </head>
 
     <body>
-        <div>
-            <h1 class="text-center mt-5">Admin Dashboard</h1>
-            <p class="text-center">Welcome to the admin dashboard. Here you can manage the website content and settings.</p>
-        </div>
 
         <!-- Quick links -->
-        <div class="container">
-            <div class="card" style="width: 20rem; height: 10rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+        <div style="width: 90%; align-items: center; margin: auto;">
+
+            <div class="quick-links mb-5">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <h1 class="text-center mt-5">Admin Dashboard</h1>
+                <p class="text-center">Welcome to the admin dashboard. Here you can manage the website content and settings.</p>
+            </div>
+
+            <div class="charts me-0">
+                <div class="bar-Charts">
+                    <div class="card mb-5 chart-card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                        </div>
+                    </div>
+
+                    <div class="card chart-card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="circle">
+                    <div class="card circle-card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </body>
-
-    <footer>
-        @include('layouts.footer')
-    </footer>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
--- resources/views/layouts/offcanvas.blade.php
+++ resources/views/layouts/offcanvas.blade.php
@@ -1,7 +1,11 @@
 <head>
     <style>
+        body {
+            background-color: #eeeff3;
+        }
+
         .offcanvas-body {
-            background-color: #113F67;
+            background-color: #1e3c5cff;
         }
 
         .icon-card {
@@ -12,12 +16,12 @@
             justify-content: center;
             align-items: center;
             border-radius: 50%;
-            box-shadow: 10px 10px 15px rgba(0, 0, 0, 0.5);
+            box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.9);
         }
 
         .icon-bg {
-            width: 100px;
-            height: 100px;
+            width: 115px;
+            height: 115px;
             background-color: white;
             border-radius: 50%;
         }
@@ -32,9 +36,9 @@
 
         .icon-body {
             width: 60px;
-            height: 60px;
+            height: 55px;
             background-color: #1A3D64;
-            border-radius: 50% / 50%;
+            border-radius: 50% /30%;
             margin: 5px auto 0 auto;
         }
 
@@ -93,9 +97,28 @@
         }
 
         @media (max-width: 768px) {
-
             .offcanvas.half-screen {
                 width: 60% !important;
+            }
+
+            .icon-card {
+                width: 100px;
+                height: 100px;
+            }
+
+            .icon-bg {
+                width: 80px;
+                height: 80px;
+            }
+
+            .icon-head {
+                width: 25px;
+                height: 25px;
+            }
+
+            .icon-body {
+                width: 40px;
+                height: 35px;
             }
 
         }
@@ -107,23 +130,26 @@
         <div style="height: 50px;">
         </div>
         <div>
-            <nav class="navbar bg-body-tertiary">
+            <nav class="navbar">
                 <div class="container-fluid">
                     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="offcanvas offcanvas-start half-screen" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                        <div class="offcanvas-header" style="background-color: #113F67; display: inline-block;">
-                            <div style="display: flex; justify-content: center;">
-                                <div class="icon-card mt-3 mb-3">
-                                    <div class="icon-bg">
-                                        <div class="icon-head"></div>
-                                        <div class="icon-body"></div>
+                        <div class="offcanvas-header" style="background-color: #1e3c5cff; display: inline-block;">
+                            <div>
+                                <div style="display: flex; justify-content: center;">
+                                    <div class="icon-card mt-3 mb-3">
+                                        <div class="icon-bg">
+                                            <div class="icon-head mt-3"></div>
+                                            <div class="icon-body mt-2"></div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Dashboard</h5>
-                            <!-- <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button> -->
+                            <div class="mt-2" style="display: flex; justify-content: center;">
+                                <h3 style="color: white;" class="offcanvas-title" id="offcanvasNavbarLabel">Admin</h3>
+                            </div>
                         </div>
                         <div class="offcanvas-body">
                             <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
